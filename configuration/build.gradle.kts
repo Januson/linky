e7328ded78d6960867f8e5885db2b38e8e7e30e7
Ondrej Janus <janusonx@gmail.com>
2020-12-14 23:25:23 +0100

    Setup bootjar

--- configuration/build.gradle.kts
+++ configuration/build.gradle.kts
@@ -1,5 +1,9 @@
+//import org.asciidoctor.gradle.AsciidoctorTask
+
 plugins {
+    application
     id("org.springframework.boot") version "2.3.2.RELEASE"
+//    id("org.asciidoctor.convert") version "1.5.9.2"
 }
 
 dependencies {
@@ -15,5 +19,49 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+//    asciidoctor 'org.springframework.restdocs:spring-restdocs-asciidoctor:{project-version}'
+//    testCompile 'org.springframework.restdocs:spring-restdocs-mockmvc:{project-version}'
+
     runtimeOnly("com.h2database:h2")
+}
+
+val appName = rootProject.name
+val appVersion = project.version.toString()
+val artifactName = "$appName-$appVersion.jar"
+
+application {
+    mainClassName = "linky.configuration.LinkyApplication"
+    applicationName = appName
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            artifact = artifactName
+            version = appName
+            name = rootProject.name
+        }
+    }
+}
+
+tasks {
+
+    bootJar {
+        archiveBaseName.set(appName)
+        archiveVersion.set(appVersion)
+
+        if (project.hasProperty("archiveName")) {
+            archiveFileName.set(project.properties["archiveName"] as String)
+        }
+    }
+
+//    "asciidoctor"(AsciidoctorTask::class) {
+//        dependsOn(":test")
+//        inputs.dir("build/generated-snippets")
+////        sourceDir = file("docs")
+//        sources(delegateClosureOf<PatternSet> {
+//            include("toplevel.adoc", "another.adoc", "third.adoc")
+//        })
+//        outputDir = file("build/docs")
+//    }
 }
