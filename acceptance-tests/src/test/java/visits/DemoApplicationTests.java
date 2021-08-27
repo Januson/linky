bc63ba555faaf9554bfee9169048290ab425a985
Ondrej Janus <janusonx@gmail.com>
2021-08-27 09:37:27 +0200

    Fix checkstyle

--- acceptance-tests/src/test/java/visits/DemoApplicationTests.java
+++ acceptance-tests/src/test/java/visits/DemoApplicationTests.java
@@ -23,77 +23,77 @@
 //@Testcontainers
 //class DemoApplicationTests {
 //
-//	private static final Future<String> IMAGE_FUTURE = new LazyFuture<>() {
-//		@Override
-//		protected String resolve() {
-//			// Find project's root dir
-//			Path cc = Paths.get(".").toAbsolutePath().getParent().getParent();
-//			File cwd = cc.toFile();
+//    private static final Future<String> IMAGE_FUTURE = new LazyFuture<>() {
+//        @Override
+//        protected String resolve() {
+//            // Find project's root dir
+//            Path cc = Paths.get(".").toAbsolutePath().getParent().getParent();
+//            File cwd = cc.toFile();
 ////			for (
 ////				cwd = new File(".");
 ////				!new File(cwd, "settings.gradle").isFile();
 ////				cwd = cwd.getParentFile()
 ////			);
 //
-//			// Make it unique per folder (for caching)
-//			var imageName = String.format(
-//				"local/app-%s:%s",
-//				DigestUtils.md5DigestAsHex(cwd.getAbsolutePath().getBytes()),
-//				System.currentTimeMillis()
-//			);
+//            // Make it unique per folder (for caching)
+//            var imageName = String.format(
+//                "local/app-%s:%s",
+//                DigestUtils.md5DigestAsHex(cwd.getAbsolutePath().getBytes()),
+//                System.currentTimeMillis()
+//            );
 //
-//			// Run Gradle task and override the image name
-//			GradleRunner.create()
-//				.withProjectDir(cwd)
-//				.withArguments("-q", "bootBuildImage", "--imageName=" + imageName)
+//            // Run Gradle task and override the image name
+//            GradleRunner.create()
+//                .withProjectDir(cwd)
+//                .withArguments("-q", "bootBuildImage", "--imageName=" + imageName)
 ////				.withArguments("-q", "bootBuildImage", "-PdockerImageName=" + imageName)
-//				.forwardOutput()
-//				.build();
+//                .forwardOutput()
+//                .build();
 //
-//			return imageName;
-//		}
-//	};
+//            return imageName;
+//        }
+//    };
 //
-//	@Container
-//	static final GenericContainer<?> APP = new GenericContainer<>(IMAGE_FUTURE)
-//		.withExposedPorts(8080)
-//		// Forward logs
-//		.withLogConsumer(new ToStringConsumer() {
-//			@Override
-//			public void accept(OutputFrame outputFrame) {
-//				if (outputFrame.getBytes() != null) {
-//					try {
-//						System.out.write(outputFrame.getBytes());
-//					} catch (IOException e) {
-//						throw new RuntimeException(e);
-//					}
-//				}
-//			}
-//		});
+//    @Container
+//    static final GenericContainer<?> APP = new GenericContainer<>(IMAGE_FUTURE)
+//        .withExposedPorts(8080)
+//        // Forward logs
+//        .withLogConsumer(new ToStringConsumer() {
+//            @Override
+//            public void accept(OutputFrame outputFrame) {
+//                if (outputFrame.getBytes() != null) {
+//                    try {
+//                        System.out.write(outputFrame.getBytes());
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }
+//        });
 //
-//	WebTestClient webClient;
+//    WebTestClient webClient;
 //
-//	@BeforeEach
-//	void setUp() {
-//		var endpoint = String.format(
+//    @BeforeEach
+//    void setUp() {
+//        var endpoint = String.format(
 //            "http://%s:%d/",
 //            APP.getContainerIpAddress(),
 //            APP.getFirstMappedPort()
 //        );
-//		webClient = WebTestClient.bindToServer().baseUrl(endpoint).build();
-//	}
+//        webClient = WebTestClient.bindToServer().baseUrl(endpoint).build();
+//    }
 //
-//	@Test
-//	public void starts() {
+//    @Test
+//    public void starts() {
 //
-//	}
+//    }
 //
-//	@Test
-//	public void healthy() {
-//		webClient.get()
-//				.uri("/actuator/health")
-//				.exchange()
-//				.expectStatus()
-//				.is2xxSuccessful();
-//	}
+//    @Test
+//    public void healthy() {
+//        webClient.get()
+//            .uri("/actuator/health")
+//            .exchange()
+//            .expectStatus()
+//            .is2xxSuccessful();
+//    }
 //}
